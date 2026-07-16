ba6e04ec0ced140952fcd7664b0567ef794cf8ab
Angel Mauricio <lecarnaquemauricioangel@gmail.com>
2026-07-16 12:41:16 -0500

    Bloque C — ComandasPage con OrderForm integrado y funcionando

---
+++ entregables/restaurante-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import ComandasPage from "./pages/ComandasPage";
+
+function App() {
+  return (
+    <div>
+      {/* El resto de sus componentes anteriores */}
+      <ComandasPage />
+    </div>
+  );
+}
+
+export default App;
