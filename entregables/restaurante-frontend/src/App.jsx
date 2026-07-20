a5ccc414a64f62d0560eeb3a560d89651034b27a
Angel Mauricio <lecarnaquemauricioangel@gmail.com>
2026-07-20 13:41:27 -0500

    archivo guardado dia 7

--- entregables/restaurante-frontend/src/App.jsx
+++ entregables/restaurante-frontend/src/App.jsx
@@ -1,57 +1,32 @@
-<<<<<<< HEAD
-import PlatoCard from "./components/PlatoCard.jsx";
-import { platosMock } from "./data/platos.mock.js";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import MenuPage from "./pages/MenuPage";
+import MesasPage from "./pages/MesasPage";
+import CarritoPage from "./pages/CarritoPage";
 
-function App() {
-
-    return (
-
-        <>
-
-            <h1>Restaurante El Sabor Peruano</h1>
-
-            {
-
-                platosMock.map(plato => (
-
-                    <PlatoCard
-
-                        key={plato.id}
-
-                        plato={plato}
-
-                    />
-
-                ))
-
-            }
-
-        </>
-
-    );
-
-}
-=======
-import { useState } from 'react';
-import NavBar from './components/NavBar';
-import Home from './pages/Home';
-import MenuPage from './pages/MenuPage';
-import MesasPage from './pages/MesasPage';
-import CarritoPage from './pages/CarritoPage';
+import { getPlatos } from "./services/api";
 
 export default function App() {
-  const [pagina, setPagina] = useState('home');
+  const [pagina, setPagina] = useState("home");
+
+  useEffect(() => {
+    async function probarAPI() {
+      const platos = await getPlatos();
+      console.log(platos);
+    }
+
+    probarAPI();
+  }, []);
 
   return (
     <>
       <NavBar cambiarPagina={setPagina} />
->>>>>>> 9fe5d6d9621a7436459d970b282ebc9fcab8b767
 
-      <main style={{ padding: '30px' }}>
-        {pagina === 'home' && <Home />}
-        {pagina === 'menu' && <MenuPage />}
-        {pagina === 'mesas' && <MesasPage />}
-        {pagina === 'carrito' && <CarritoPage />}
+      <main style={{ padding: "30px" }}>
+        {pagina === "home" && <Home />}
+        {pagina === "menu" && <MenuPage />}
+        {pagina === "mesas" && <MesasPage />}
+        {pagina === "carrito" && <CarritoPage />}
       </main>
     </>
   );
